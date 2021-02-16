471e85d0717c05f2b9cfbd372a0be8e969a2400b
Mooooooov <mateusvasconcelos4210@gmail.com>
2021-02-16 09:33:57 -0300

    Changing the size of the container/fonts

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const transition_el = document.querySelector(".transition");
-const inputUser = document.getElementById("user");
+const transition_heart = document.querySelector(".transition-heart");
 
 function transitionContainer() {
   setTimeout(() => {
     transition_el.classList.remove("is-active");
+  }, 100);
+}
+
+function transitionHeart() {
+  setTimeout(() => {
+    transition_heart.classList.remove("is-active");
   }, 100);
 }
